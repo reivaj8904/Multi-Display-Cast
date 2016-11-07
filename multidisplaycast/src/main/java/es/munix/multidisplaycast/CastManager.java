df277d1db3e9bceee112c7cf146d44f5c342070a
munix <jpmuniz@gmail.com>
2016-11-07 18:56:44 +0100

    Log

--- multidisplaycast/src/main/java/es/munix/multidisplaycast/CastManager.java
+++ multidisplaycast/src/main/java/es/munix/multidisplaycast/CastManager.java
@@ -408,6 +408,7 @@ public class CastManager implements DiscoveryManagerListener, MenuItem.OnMenuIte
 
                         @Override
                         public void onError( ServiceCommandError error ) {
+                            error.printStackTrace();
                             stop();
                             Toast.makeText( activity, "Contenido no compatible", Toast.LENGTH_LONG )
                                     .show();
